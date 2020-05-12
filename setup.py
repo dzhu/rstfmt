19406266b4c19bab063303b3b37c60d976b792ae
Danny Zhu <dzhu@dzhu.us>
2020-05-11 23:39:53 -0700

    Bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.rst")
 
 setup(
     name="rstfmt",
-    version="0.0.2",
+    version="0.0.3",
     author="Danny Zhu",
     author_email="dzhu@dzhu.us",
     url="https://github.com/dzhu/rstfmt",
