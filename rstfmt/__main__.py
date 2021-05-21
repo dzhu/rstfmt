64f375eb70ad8a511773e1a85f5b8edf21e67e98
Danny Zhu <dzhu@dzhu.us>
2021-05-20 22:19:43 -0700

    Refactor main file to add a function for handling one input file

--- rstfmt/__main__.py
+++ rstfmt/__main__.py
@@ -19,6 +19,38 @@ class nullcontext(contextlib.AbstractContextManager):  # type: ignore
         pass
 
 
+STDIN = "-"
+
+
+def do_file(args, fn, misformatted):
+    cm = cast(ContextManager[TextIO], nullcontext(sys.stdin) if fn == STDIN else open(fn))
+    with cm as f:
+        inp = f.read()
+    doc = rstfmt.parse_string(inp)
+
+    if args.verbose:
+        print("=" * 60, fn, file=sys.stderr)
+        debug.dump_node(doc, sys.stderr)
+
+    if args.test:
+        try:
+            debug.run_test(doc)
+        except AssertionError as e:
+            raise AssertionError(f"Failed consistency test on {fn}!") from e
+        return
+
+    output = rstfmt.format_node(args.width, doc)
+
+    if args.check:
+        if output != inp:
+            misformatted.append("Standard input" if fn == STDIN else fn)
+        return
+
+    cm = cast(ContextManager[TextIO], nullcontext(sys.stdout) if fn == STDIN else open(fn, "w"))
+    with cm as f:
+        f.write(output)
+
+
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", action="store_true")
@@ -35,41 +67,10 @@ def main() -> None:
 
     rst_extras.register()
 
-    STDIN = "-"
     misformatted = []
 
     for fn in args.files or [STDIN]:
-        cm = cast(ContextManager[TextIO], nullcontext(sys.stdin) if fn == STDIN else open(fn))
-
-        with cm as f:
-            inp = f.read()
-        doc = rstfmt.parse_string(inp)
-
-        if args.verbose:
-            print("=" * 60, fn, file=sys.stderr)
-            debug.dump_node(doc, sys.stderr)
-
-        if args.test:
-            try:
-                debug.run_test(doc)
-            except AssertionError as e:
-                raise AssertionError(f"Failed consistency test on {fn}!") from e
-            continue
-
-        output = rstfmt.format_node(args.width, doc)
-
-        if args.check:
-            if output != inp:
-                misformatted.append("Standard input" if fn == STDIN else fn)
-            continue
-
-        if fn != STDIN:
-            cm = open(fn, "w")
-        else:
-            cm = nullcontext(sys.stdout)
-
-        with cm as f:
-            f.write(output)
+        do_file(args, fn, misformatted)
 
     if misformatted:
         for fn in misformatted:
